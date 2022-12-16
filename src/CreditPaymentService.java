8d23a095c7a5bfbe0db902fb2863b7959d248972
Margarita Kosareva <kosareva-r@ya.ru>
2022-12-16 10:17:07 +0300

    CreditPaymentService chenged

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,14 @@
 public class CreditPaymentService {
-    public double calculate(int creditAmount, double monthPercent, int month) {
-        double x = 1 + monthPercent;
+
+    //double monthPercent процентная ставка в месяц
+    //double k Коэффициент аннуитета
+    public double calculate(int creditAmount, double percent, int month) {
+        double monthPercent = percent / 12 / 100;
+        double x = 1 +  monthPercent;
         double y = Math.pow(x, month);
-        double monthPayment = creditAmount * ((monthPercent * y) / (y - 1));
+        double k = monthPercent * y / (y - 1);
+        double monthPayment = creditAmount * k;
+
         return monthPayment;
     }
 }
